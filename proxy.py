3e06c9d22be06e09d7b8fb681b49039c9048b6fc
Thomas Ballinger <thomasballinger@gmail.com>
2012-06-29 17:10:38 -0400

    proxy working very slowly

--- proxy.py
+++ proxy.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python
-import os
-import subprocess
-import time
 import re
 import socket
 import sys
-import mimetypes
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email import utils, encoders
-from gevent import monkey
-
-#monkey.patch_all()
 
 var = {}
 port = 8000
@@ -30,26 +19,43 @@ def main():
     print "Connected on port", port
     serversocket.listen(5)
 
-    while 1: 
-        client, address = serversocket.accept() 
+    while 1:
+        client, address = serversocket.accept()
         print "Request recieved."
-        client_data = client.recv(10240) 
-        print client_data
+        client_data = client.recv(10240)
+        if not client_data:
+            client.close()
+            continue
+        print "Request read"
+        print 'client_data:'
+        print client_data[:400]
         key = tuple(re.split(r"[\r\n]+", client_data)[0:2])
         if key in cache:
             pass
+        print 'caching key:'
         print key
+        print '-----'
         full_host_string = key[1][6:]
         host = full_host_string
+        print 'host:', host
         host_port = 80
         host_sock = socket.socket()
         host_sock.connect((host, host_port))
-        host_sock.send(client_data)
-        host_data = host_sock.recv(1024 * 1024)
+        print 'send returns:'
+        print host_sock.send(client_data.replace('I like dogs', 'I hate dogs'))
+        print 'should equal:'
+        print len(client_data)
+
+        while True:
+            host_data = host_sock.recv(1000)
+            if not host_data:
+                break
+            print 'data from server recieved:', len(host_data)
+
+            print 'amound of data sent to client:', client.send(host_data)
+            print 'we should have just sent data...'
         host_sock.close()
-        client.send(host_data)
         client.close()
 
-    return ret
 if __name__ == "__main__":
     main()
