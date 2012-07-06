bd34795aea42260ee22b6b31cb33e23543280f5c
Ingrid <hi@ingridcheung.com>
2012-07-06 17:45:50 -0400

    Checkpoint.

--- asyncproxy/Loop.py
+++ asyncproxy/Loop.py
@@ -8,38 +8,40 @@ import Stream
 
 logger = logging.getLogger(__name__)
 
-kq_timeout = 1000
+kq_timeout = 10
+POLL_TIMEOUT = 2
 
 class Loop(object):
 
     def __init__(self):
         self.streams = {}
         # Add epoll and other support.
-        self.queue = KQ()
+        self.poll = KQ()
         self.curr_streams = {}
 
-    def add_stream(self, file_descriptor, stream):
+    def add_stream(self, file_descriptor, stream, events):
         self.curr_streams[file_descriptor] = stream
-        # Need an alias so epoll is used on norma Linux.
-        self.queue.register(file_descriptor, stream)
+        self.poll.register(file_descriptor, events)
 
-    def add_socket(self, socket):
+    def add_socket(self, socket, events):
         file_descriptor = socket.fileno;
         stream = Stream(socket)
 
-        return self.add_stream(file_descriptor, stream)
+        return self.add_stream(file_descriptor, stream, events)
 
     def run(self):
         self.stop = False
 
         while not self.stop:
-            print "Polling."
-            events = select.poll()
+            print "Polling..."
+            events = self.poll.poll()
+            print "Got events!"
             while events:
                 file_descriptor, event = events.popitem()
                 print "File descriptor: ", file_descriptor
                 print "Event: ", event
-            time.sleep(kq_timeout)
+            print "Sleeping..."
+            time.sleep(POLL_TIMEOUT)
 
 # A kqueue wrapper.
 class KQ(object):
@@ -53,8 +55,19 @@ class KQ(object):
     def close(self):
         self.kq.close()
 
-    def register(self, fd, events):
+    def register(self, fd, events=[]):
         if fd in self.active.keys():
             print "File descriptor already registered. Passing."
+            return
+        print "Registering a new connection."
         self.active[fd] = events
+        kevents = []
+        kevents.append(select.kevent(fd.fileno(), events))
+        print self.kq.control([], 0, None)
+
+    def poll(self):
+        # Currently returns one at a time.
+        # Need to update active.
+        events = self.kq.control(None, 1, None)
+        return events
                 
